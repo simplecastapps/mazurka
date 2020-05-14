05a291420a0668481330eb07498ee60627e7613c
David McHealy <david@simplecast.com>
2020-05-14 16:34:26 -0400

    Forgot validation / condition do block notation.

--- lib/mazurka/resource/condition.ex
+++ lib/mazurka/resource/condition.ex
@@ -3,7 +3,13 @@ defmodule Mazurka.Resource.Condition do
 
   use Mazurka.Resource.Utils.Check
 
-  defmacro condition(block, message \\ nil) do
+  defmacro condition(block, message \\ nil)
+
+  defmacro condition([do: block], message) do
+    Scope.check(:condition, block, if message do message else block |> Macro.to_string() end)
+  end
+
+  defmacro condition(block, message) do
     Scope.check(:condition, block, if message do message else block |> Macro.to_string() end)
   end
 end
--- lib/mazurka/resource/validation.ex
+++ lib/mazurka/resource/validation.ex
@@ -3,7 +3,13 @@ defmodule Mazurka.Resource.Validation do
 
   use Mazurka.Resource.Utils.Check
 
-  defmacro validation(block, message \\ nil) do
+  defmacro validation(block, message \\ nil)
+
+  defmacro validation([do: block], message) do
+    Scope.check(:validation, block, if message do message else block |> Macro.to_string() end)
+  end
+
+  defmacro validation(block, message) do
     Scope.check(:validation, block, if message do message else block |> Macro.to_string() end)
   end
 end
