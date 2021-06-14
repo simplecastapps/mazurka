0738b29b61940a12eae14872ed3dc66852954d2b
David McHealy <david@simplecast.com>
2021-06-14 09:22:39 -0400

    Fix an issue where optioned input defaults were sent to the linked route.

--- lib/mazurka/resource/option.ex
+++ lib/mazurka/resource/option.ex
@@ -34,15 +34,45 @@ defmodule Mazurka.Resource.Option do
       |> Enum.reverse()
       |> Mazurka.Resource.Utils.Scope.filter_by_options()
       |> Mazurka.Resource.Utils.Scope.filter_by_bindings()
-      |> Enum.map(fn {_var, name, _, _, _, _, _default, _} ->
-        {name, Macro.var(name, nil)}
+      |> Enum.map(fn
+          {_var, name, :input, _, _, _, default, _} ->
+        if default == :__mazurka_unspecified do
+          {name, {:input, Utils.hidden_var(name)}}
+        else
+          {name, {:input, Macro.var(name, nil)}}
+        end
+
+        {_var, name, input_type, _, _, _, _default, _} ->
+          {name, {input_type, Macro.var(name, nil)}}
       end)
 
     if type == :atom do
-      {:%{}, [], all_options}
+      # We have to filter out inputs that had option: true from
+      # being passed to the next route if the user did not submit
+      # them because they would pass their default in place of
+      # whatever the user sent. Better to let the next route deal
+      # with their absense in a way that makes sense to that route.
+      quote do
+        supplied_inputs = unquote(Utils.input()) |> Map.keys()
+
+        unquote(all_options) |> Enum.filter(fn
+          {k, {:input, v}} -> k in supplied_inputs
+          _ -> true
+        end)
+        |> Enum.map(fn {k, {_, v}} -> {k, v} end) |> Map.new()
+      end
     else
+      quote do
+        supplied_inputs = unquote(Utils.input()) |> Map.keys()
+
+        unquote(all_options) |> Enum.filter(fn
+          {k, {:input, v}} -> k in supplied_inputs
+          _ -> true
+        end)
+        |> Enum.map(fn {k, {_, v}} -> {k |> to_string(), v} end) |> Map.new()
+      end
+
       all_options = all_options |> Enum.map(fn {k, v} -> {to_string(k), v} end)
-      {:%{}, [], all_options}
     end
   end
 
